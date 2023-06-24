078b6e75515638a8cbcdcc54b4bf6d8e830f8546
Adson <Adsonfr.vieira@hotmail.com>
2023-06-24 16:04:37 -0300

    Cliente Dto

---
+++ src/main/java/Dto/ClienteDto.java
@@ -0,0 +1,34 @@
+package Dto;
+
+
+	public class ClienteDto{
+		
+		
+		private Integer id;
+		
+	
+		private String nome;
+		
+		
+		private String email;
+		
+		
+		private int telefone;
+		
+		
+		private int cep;
+		
+		private String endereco;
+		
+		
+		private String cidade;
+		
+		private int  numero;
+		
+		
+		
+
+	}
+
+
+}
