0dc407912142b335319dc915d5a94fac5f0cbd82
Adson <Adsonfr.vieira@hotmail.com>
2023-07-06 21:14:34 -0300

    remove modelMapper

--- src/main/java/br/com/Produto/service/Cliente/IClienteService.java
+++ src/main/java/br/com/Produto/service/Cliente/IClienteService.java
@@ -2,18 +2,16 @@ package br.com.Produto.service.Cliente;
 
 import java.util.List;
 
-import br.com.Produto.dto.ClienteDto;
 import br.com.Produto.model.Cliente;
+
 
 
 public interface IClienteService {
 	
-	public Cliente salvar(Cliente cliente);
+	public Cliente salvar(Cliente clienteDto);
 	public Cliente alterar(Cliente alterar);
 	public void   deletar( int deleta);
 	public List<Cliente> listartodos();
 	public Cliente buscar(int id); 
-	public ClienteDto ClienteMapper(Integer id);
-	public  List<ClienteDto> listMapper();
 
 }
--- src/main/java/br/com/Produto/service/Cliente/ServiceClienteImpl.java
+++ src/main/java/br/com/Produto/service/Cliente/ServiceClienteImpl.java
@@ -2,62 +2,48 @@ package br.com.Produto.service.Cliente;
 
 import java.util.List;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.Produto.Dao.ClienteDAO;
-import br.com.Produto.dto.ClienteDto;
 import br.com.Produto.model.Cliente;
 
 @Component
 public class ServiceClienteImpl implements IClienteService {
 
 	@Autowired
-	private  ClienteDAO dao ;
+	private ClienteDAO  dao;
 	
 	@Override
 	public Cliente salvar(Cliente cliente) {
-		
+		// TODO Auto-generated method stub
 		return dao.save(cliente);
 	}
 
 	@Override
 	public Cliente alterar(Cliente alterar) {
-		
+		// TODO Auto-generated method stub
 		return dao.save(alterar);
 	}
 
 	@Override
 	public void deletar(int deleta) {
-	
 		dao.deleteById(deleta);
 		
 	}
 
 	@Override
 	public List<Cliente> listartodos() {
-		
-		return dao.findByOrderByNomeAsc();
+		// TODO Auto-generated method stub
+		  return dao.findAll();
 	}
 
 	@Override
-  public Cliente buscar(int id) {
-	
+	public Cliente buscar(int id) {
+		// TODO Auto-generated method stub
 		return dao.findById(id).orElse(null);
 	}
-
-	@Override
-	public ClienteDto ClienteMapper(Integer id) {
-		
-		Cliente cliente =  dao.findById(id).orElse(null);
-		
-		ModelMapper modelMapper = new ModelMapper();
-		
-		ClienteDto clienteDto = modelMapper.map(cliente, ClienteDto.class);
-		
-		return clienteDto;
-	}
-
+	
+	
 	
 }
